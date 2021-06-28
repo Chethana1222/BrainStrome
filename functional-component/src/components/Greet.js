bc334ad6cc006ba657fae5274cbb0353d3a99022
Krishnamurthy, Chethana <Chethana.Krishnamurthy2@Cerner.com>
2021-06-28 21:18:36 +0530

    Update: Arrow function usage, default exports and named exports usage

--- functional-component/src/components/Greet.js
+++ functional-component/src/components/Greet.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
-function Greet(){
-    return <h1>Hello CK !!!</h1>
-}
+// function Greet(){
+//     return <h1>Hello CK !!!</h1>
+// }
+
+const Greet = () => <h1>Hello CK !!!</h1>;
 
 export default Greet;
+/* 
+Default exports
+export default Greet;
+Can be imported with any names 
+Eg: import Greet from './components/Greet'
+Eg: import myGreetComponent from './components/Greet'
+*/
+
+/* Named export
+ export const Greet = () => <h1>Hello CK !!!</h1>;
+ in the file imported with Greet only
+ Eg: import Greet from './components/Greet'
+*/
